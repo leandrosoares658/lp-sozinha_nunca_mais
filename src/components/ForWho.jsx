1c650d9ced2524d364bd76da658beb534eea0319
unknown <leandrosoares@gmail.com>
2025-12-30 14:18:29 -0300

    Versão 1.5

--- src/components/ForWho.jsx
+++ src/components/ForWho.jsx
@@ -3,50 +3,50 @@ export default function ForWho() {
         <section className="forwho">
             <div className="forwho-container">
                 <h2>
-                    Para quem é o{" "}
-                    <span className="bold">#SozinhaNuncaMais</span>?
+                    <span className="first">Para Quem É O</span> <br />
+                    <span className="second">#Sozinha Nunca Mais?</span>
                 </h2>
 
-                <div className="forwho-grid">
-                    <div className="forwho-item">
-                        <p>
-                            Mulheres acima dos 40 anos que estão cansadas de se envolver em
-                            relações rasas e sem futuro.
-                        </p>
-                    </div>
+                <h3>
+                    Este guia é especialmente desenvolvido para mulheres que desejam
+                    transformar sua vida amorosa e encontrar um relacionamento verdadeiro.
+                </h3>
 
-                    <div className="forwho-item">
-                        <p>
-                            Mulheres que querem um relacionamento sério, leve e verdadeiro: 
-                            sem jogos emocionais.
-                        </p>
-                    </div>
-
-                    <div className="forwho-item">
-                        <p>
-                            Mulheres que desejam se posicionar com segurança e serem
-                            valorizadas.
-                        </p>
-                    </div>
-
-                    <div className="forwho-item">
-                        <p>
-                            Mulheres que não querem mais perder tempo com promessas vazias.
-                        </p>
-                    </div>
-
-                    <div className="forwho-item">
-                        <p>
-                           Mulheres que querem recomeçar, vivendo uma nova fase cheia de amor, segurança e possibilidades.
-                        </p>
-                    </div>
-
-                    <div className="forwho-item">
-                        <p>
-                            Mulheres que querem resgatar a autoconfiança e voltar a acreditar no amor.
-                        </p>
-                    </div>
-                </div>
+                <ul className="forwho-grid">
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres acima dos 40 anos que estão cansadas de se envolver em
+                            relações rasas e sem futuro.</span>
+                    </li>
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres que querem um relacionamento sério, leve e verdadeiro:
+                            sem jogos emocionais.</span>
+                    </li>
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres que desejam se posicionar com segurança e serem
+                            valorizadas.</span>
+                    </li>
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres que não querem mais perder tempo com promessas vazias.</span>
+                    </li>
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres que querem recomeçar, vivendo uma nova fase cheia de amor, segurança e possibilidades.</span>
+                    </li>
+                    <li className="forwho-item">
+                        <span className="icon">
+                            <svg aria-hidden="true" className="e-font-icon-svg" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>						</span>
+                        <span className="list-text">Mulheres que querem resgatar a autoconfiança e voltar a acreditar no amor.</span>
+                    </li>
+                </ul>
 
                 <a href="https://pay.cakto.com.br/3ea3b5d_559921"
                     target="_blank" className="cta">
@@ -54,6 +54,6 @@ export default function ForWho() {
                 </a>
 
             </div>
-        </section>
+        </section >
     );
 }
